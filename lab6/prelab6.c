3f9387c400b5cfa5dbece0ff90ff54e7da36ee41
Matt Marlow <thequantumblink@gmail.com>
2021-10-07 16:35:37 -0500

    prelab6

---
+++ lab6/prelab6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define SIZE 256
+
+int initializeArray(int array[], int begin, int end, int increment);
+int printArray(int array[], int size);
+
+int main(void) {
+    //int size = 0;
+    int myArray[SIZE];
+    // this may not work, but I am curious so I want to try it
+    printArray(myArray, initializeArray(myArray, 2, 20, 2));
+    printArray(myArray, initializeArray(myArray, 1, 97, 3));
+    return 0;
+}
+int initializeArray(int array[], int begin, int end, int increment) {
+    int i =0;
+    for (; begin <= end; begin+=increment) {
+        array[i] = begin;
+        i++;
+    }
+    return i;
+}
+int printArray(int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("Element %d = %d\n", i, array[i]);
+    }
+    return 0;
+}
